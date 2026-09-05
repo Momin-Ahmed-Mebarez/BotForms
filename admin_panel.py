337869104b7b338fc2118f4a77a9c3d5dc5d98f6
Momin-Ahmed-Mebarez <momenmomen35@gmail.com>
2026-09-05 09:27:03 +0300

    Uncommented a change

--- admin_panel.py
+++ admin_panel.py
@@ -10,5 +10,7 @@ connection = sqlite3.connect(HOME / "forum.db",timeout=30)
 test = auth.generate_api_key()
 print(test)
 
-#dbHandle.register_author(connection,"The boss",test["hashed_key"])
+dbHandle.register_author(connection,"The boss",test["hashed_key"])
+
+
 #dbHandle.update_webhook(connection,3,"local")
